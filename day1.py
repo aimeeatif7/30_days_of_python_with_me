5f4b7e9bdfcbf250a20658023e9fa04f8516dcd4
aimeeatif7 <aimanatif107@gmail.com>
2026-05-21 22:09:10 +0500

    Add two new files

---
+++ day1.py
@@ -0,0 +1,29 @@
+a=3
+b=4
+print("addition:",a+b)
+print("subtraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a/b)
+print("modulus:",a%b)
+print("exponentiation:",a**b)
+print("floor division:",a//b)
+
+print("Aiman\nRana\nPakistan\nI am enjoying 30 days of python.")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Aiman"))
+print(type("Rana"))
+print(type("Pakistan"))
+
+age=21
+price=34.6
+result=3+4j
+name="Aiman"
+is_Student=True
+fruits=["apple","orange","kiwi","mango","strawberry"]
+subjects=("mathematics","science","english")
+five_num={0,1,2,3,4,5}
